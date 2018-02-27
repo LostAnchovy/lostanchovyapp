f8903d43a28ad3a0d096fb161154ac48a7d912d9
Keith Nguyen <khoacn@gmail.com>
2018-02-27 13:10:14 -0800

    Initial Commit

---
+++ The Lost AnchovyTests/The_Lost_AnchovyTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Lost_AnchovyTests.swift
+//  The Lost AnchovyTests
+//
+//  Created by keith Nguyen on 2/27/18.
+//  Copyright © 2018 Lost_Anchovy LLC. All rights reserved.
+//
+
+import XCTest
+@testable import The_Lost_Anchovy
+
+class The_Lost_AnchovyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
